4db5b113ab14d0d77ac57d53d7d5e78caf67b298
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-08-21 17:08:26 -0300

    Adding the sell total calculation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
   include Fae::BaseModelConcern
+  belongs_to :supplier, optional: true
   has_fae_image :hero_image
 
   def fae_display_field
     title
   end
 
-  belongs_to :supplier
 end
--- app/models/sell.rb
+++ app/models/sell.rb
@@ -7,8 +7,13 @@ class Sell < ApplicationRecord
   has_many :sell_services
   has_many :services, through: :sell_services
 
+  belongs_to :discount, optional: true
+  belongs_to :client
+
   enum status: { finished: 0, canceled: 1 }
-  validates :client, :products, :services, presence: true
+  validates :client, presence: true
+
+  before_save :set_total
   
   def fae_display_field
     id
@@ -18,6 +23,18 @@ class Sell < ApplicationRecord
     order(:id)
   end
 
-  belongs_to :discount
-  belongs_to :client
+  private
+
+  def set_total
+    total = 0
+    self.products.each { |product| total += product.price }
+    self.services.each { |service| total += service.price }
+
+    if self.discount.present?
+      total -= self.discount.value
+    end
+    
+    total = total > 0 ? total : 0
+    self.total = total
+  end
 end
